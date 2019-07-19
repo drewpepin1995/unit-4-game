37e9cfa196182c71af0985785ab66f7d8de41823
Drew Pepin <drewpepin1995@gmail.com>
2019-07-19 16:10:40 -0400

    generating values for each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,63 @@
+let wins = 0;
+let losses = 0;
+let crystalOneValue = 0;
+let crystalTwoValue = 0;
+let crystalThreeValue = 0;
+let crystalFourValue = 0;
+let yourScore = 0;
+
+
+
+
+
 $(document).ready(function(){
+
+    let targetScore = Math.floor((Math.random() * (100-12) + 12));
+    let crystalOneValue = Math.floor((Math.random() * (12-1) + 1));
+    let crystalTwoValue = Math.floor((Math.random() * (12-1) + 1));
+    let crystalThreeValue = Math.floor((Math.random() * (12-1) + 1));
+    let crystalFourValue = Math.floor((Math.random() * (12-1) + 1));
 
     $("#rulesBtn").on("click",function(){
         $("#rulesInfo").toggle();
     });
+
+
+    $("#crystalOne").on("click", function(){
+        $("#yourScore").html("Your score : " + crystalOneValue);
+        
+        
+
+    });
+
+    $("#crystalTwo").on("click", function(){
+        $("#yourScore").html("Your score : " + crystalTwoValue);
+        
+        
+
+    });
+
+    $("#crystalThree").on("click", function(){
+        $("#yourScore").html("Your score : " + crystalThreeValue);
+        
+        
+
+    });
+
+    $("#crystalFour").on("click", function(){
+        $("#yourScore").html("Your score : " + crystalFourValue);
+        
+        
+
+    });
+
+
+
+    $("#wins").html("Wins : " + wins);
+    $("#losses").html("Losses : " + losses);
+    $("#targetScore").html("Target Score : " + targetScore);
+    
+    
 
 
 
