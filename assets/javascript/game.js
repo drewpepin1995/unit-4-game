4b540b8abce94f23cdbb753b8dafbd7b4713d5d2
Drew Pepin <drewpepin1995@gmail.com>
2019-07-21 23:28:46 -0400

    Making some JS adjustments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,61 @@
 let wins = 0;
 let losses = 0;
+let yourScore = 0;
+let targetScore = 0;
+let newScore = 0;
 let crystalOneValue = 0;
 let crystalTwoValue = 0;
 let crystalThreeValue = 0;
 let crystalFourValue = 0;
-let yourScore = 0;
-let targetScore = 0;
+
 
 function resetGame() {
     yourScore = 0;
     targetScore = Math.floor((Math.random() * (100-12) + 12));
+    crystalOneValue = Math.floor((Math.random() * (12-1) + 1));
+	crystalTwoValue = Math.floor((Math.random() * (12-1) + 1));
+	crystalThreeValue = Math.floor((Math.random() * (12-1) + 1));
+	crystalFourValue = Math.floor((Math.random() * (12-1) + 1));
+	
+	$("#wins").html("Wins : " + wins);
+	$("#losses").html("Losses : " + losses);
+	$("#targetScore").html("Target Score : " + targetScore);
+	$("#yourScore").html("Your Score : " + yourScore);
+}
+
+function updateYourScore (newScore) {
+    yourScore = newScore;
+    $("#yourScore").html("Your Score : " + yourScore);
+}
+
+function updateWinsAndLosses() {
+    const scoreDiff = targetScore - yourScore;
+
+    if (scoreDiff === 0 || scoreDiff < 0) {
+        if (scoreDiff === 0) {
+            wins += 1;
+            alert("You win!")
+            $("#wins").html("Wins : " + wins);
+        } else {
+            losses += 1;
+            alert("You lose!")
+            $("#losses").html("Losses : " + losses);
+        }
+
+        resetGame();
+    }
+}
+
+function updateGame(newScore) {
+    updateYourScore(newScore);
+    updateWinsAndLosses();
 }
 
 
 
 $(document).ready(function(){
 
-    let targetScore = Math.floor((Math.random() * (100-12) + 12));
-    let crystalOneValue = Math.floor((Math.random() * (12-1) + 1));
-    let crystalTwoValue = Math.floor((Math.random() * (12-1) + 1));
-    let crystalThreeValue = Math.floor((Math.random() * (12-1) + 1));
-    let crystalFourValue = Math.floor((Math.random() * (12-1) + 1));
+   resetGame();
 
     $("#rulesBtn").on("click",function(){
         $("#rulesInfo").toggle();
@@ -28,31 +63,23 @@ $(document).ready(function(){
 
 
     $("#crystalOne").on("click", function(){
-        let newValue = yourScore += crystalOneValue;
-        $("#yourScore").html("Your score : " + newValue);
+        updateGame(yourScore + crystalOneValue);
         
     });
 
     $("#crystalTwo").on("click", function(){
-        let newValue = yourScore += crystalTwoValue;
-        $("#yourScore").html("Your score : " + newValue);
+        updateGame(yourScore + crystalTwoValue);
 
     });
 
     $("#crystalThree").on("click", function(){
-        let newValue = yourScore += crystalThreeValue;
-        $("#yourScore").html("Your score : " + newValue);
+        updateGame(yourScore + crystalThreeValue);
 
     });
 
     $("#crystalFour").on("click", function(){
-        let newValue = yourScore += crystalFourValue;
-        $("#yourScore").html("Your score : " + newValue);
+        updateGame(yourScore + crystalFourValue);
 
     });
-
-        $("#wins").html("Wins : " + wins);
-        $("#losses").html("Losses : " + losses);
-        $("#targetScore").html("Target Score : " + targetScore);
 
 });
